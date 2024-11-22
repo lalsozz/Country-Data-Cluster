8392a344f518fb8cc0cffbf1848f6e93fbcb0049
Bhagyeshri Oza <90242054+lalsozz@users.noreply.github.com>
2024-11-22 14:17:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle 
+
+# load instances that were created
+with open('final_model.pkl','rb') as file:
+  model = pickle.load(file)
+    
+with open('pca.pkl','rb') as file:
+  model = pickle.load(file)
+    
+with open('scaler.pkl','rb') as file:
+  model = pickle.load(file
+
+def prediction(input_data):
+
+    scaled_data=scaler.transform(input_data)
+    pca_data=pca.transform(scaled_data)
+
+    pred = model.predict(pca_data)[0]
+
+    if pred==0:
+        return 'Developing'
+    elif pred==1:
+        return 'Underdeveloped'
+    else:
+        return 'Developed'
+
+def main():
+    st.title('Help International Foundation')
+    st.subheader('This application will give the status of the country based on socio-economic factors')
+    ch_mort=st.text_input('Enter child mortality rate:')
+    exp = st.text_input('Enter Exports (%GDP):')
+    imp = st.text_input('Enter imports (%GDP):')
+    hel = st.text_input('Enter expenditure on health and(% GDP)')
+    inc = st.text_input('Enter average income')
+    inf = st.text_input('Enter inflation')
+    life_exp = st.text_input('Enter life expectancy ')
+    fer = st.text_input('Enter fertility rate')
+    gdp = st.text_input('Enter Gdp per population')
+
+    input_list=[[ch_mort,exp,imp,help,inc,inf,life_exp,fer,gdp]]
+    
+    if st.button('Predict'):
+        response=prediction(input_list)
+        st.success(response)
+
+if __name__ == '__main__'
+   main()
